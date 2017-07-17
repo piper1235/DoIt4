f53c7c41a4f96212366fd886a36784836f0bf0ce
Tony <piper1235@yahoo.com>
2017-07-17 11:52:16 -0700

    Up to label.text....then it fails.

---
+++ DoIt4/CompleteTaskViewController.swift
@@ -0,0 +1,26 @@
+//
+//  CompleteTaskViewController.swift
+//  DoIt4
+//
+//  Created by Tony on 7/17/17.
+//  Copyright © 2017 Tony. All rights reserved.
+//
+
+import UIKit
+
+class CompleteTaskViewController: UIViewController {
+    
+    @IBOutlet var taskLabel: UIView!
+    var task = Task()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+    
+    @IBAction func completeTapped(_ sender: Any) {
+    }
+    
+    
+}
--- DoIt4/TasksViewController.swift
+++ DoIt4/TasksViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class TasksViewController: UIViewController,UITableViewDelegate, UITableViewDataSource {
-
+    
     @IBOutlet weak var tableView: UITableView!
     
     var tasks : [Task] = []
@@ -41,6 +41,11 @@ class TasksViewController: UIViewController,UITableViewDelegate, UITableViewData
         return cell
     }
     
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let task = tasks[indexPath.row]
+        performSegue(withIdentifier: "selectTaskSegue", sender: task)
+    }
+    
     func makeTasks() -> [Task] {
         let task1 = Task()
         task1.name = "Walk the dog"
@@ -61,11 +66,14 @@ class TasksViewController: UIViewController,UITableViewDelegate, UITableViewData
         performSegue(withIdentifier: "addSegue", sender: nil)
     }
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        let nextVC = segue.destination as! CreateTaskViewController
-        nextVC.previousVC = self
-        
+        if segue.identifier == "addSegue" {
+            let nextVC = segue.destination as! CreateTaskViewController
+            nextVC.previousVC = self
+        }
+        if segue.identifier == "selectTaskSegue"{
+            let nextVC = segue.destination as! CompleteTaskViewController
+            nextVC.task = sender as! Task
+        }
     }
-    
-    
 }
 
